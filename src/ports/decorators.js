616db6ae66b3e59335b1ebd28b48e05f5b9ec3cd
EdSabino <eduardoaikin@gmail.com>
2021-07-09 03:38:47 -0300

    change log

--- src/ports/decorators.js
+++ src/ports/decorators.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 function database(args) {
   mongoose.connect(process.env.MONGO_URI)
-   .then(() => console.log('MongoDB connected'))
+   .then(() => console.log('Mongo connected'))
    .catch(err => console.log(err));
   create(args);
 }
