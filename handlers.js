b3e5fd7dfc7fd7f81987afb96a40e1aabf534b95
EdSabino <eduardoaikin@gmail.com>
2021-07-12 01:52:57 -0300

    add function to handler

--- handlers.js
+++ handlers.js
@@ -3,9 +3,13 @@
 const create = require('./src/ports/handlers/create');
 const get = require('./src/ports/handlers/get');
 const update = require('./src/ports/handlers/update');
+const list = require('./src/ports/handlers/list');
+const login = require('./src/ports/handlers/login');
 
 module.exports = {
   create,
   get,
-  update
+  update,
+  list,
+  login
 };
