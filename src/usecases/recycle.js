7fa48fade0b427e2cbbcfe0de2ffd9099162de1c
EdSabino <eduardoaikin@gmail.com>
2021-07-28 00:07:54 -0300

    adapt recycle

--- src/usecases/recycle.js
+++ src/usecases/recycle.js
@@ -4,6 +4,7 @@ const User = require('../ports/models/User');
 const createTokenFromUser = require('./token_from_user');
 
 exports.execute = async ({ requestContext }) => {
+  requestContext.authorizer.claims = JSON.parse(requestContext.authorizer.stringKey);
   const user = await User.findOne({ _id: requestContext.authorizer.claims._id }, User.publicFields());
   if (!user) {
     throw { success: false, message: 'user_not_found' };
