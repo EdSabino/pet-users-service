afc6351151b69ab6f846e802c1b79bc69c7057d7
EdSabino <eduardoaikin@gmail.com>
2021-09-23 00:18:04 -0300

    fixx test

--- test/User.test.ts
+++ test/User.test.ts
@@ -1,5 +1,5 @@
 import User from "../src/models/User";
-import { user } from "./mocks/user.mock";
+import { user } from "./mocks/User.mock";
 
 describe('User', () => {
   describe('with valid establishment', () => {
